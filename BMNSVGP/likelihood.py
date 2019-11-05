211ea7a2bef2122274c3f65c3bdcbc1c9abbe03a
Aidan Scannell <scannell.aidan@gmail.com>
2019-11-05 16:20:13 +0000

    Added more comments

--- BMNSVGP/likelihood.py
+++ BMNSVGP/likelihood.py
@@ -26,21 +26,33 @@ def inv_probit(x):
 
 
 class BernoulliGaussian(Likelihood):
-    # def __init__(self, variance_low=0.005, variance_high=0.3, invlink=inv_probit, **kwargs):
+    """
+    Implementation of the following likelihood:
+
+    p(Y | F, a) = {\prod_{n=1}^{N}} \mathcal{N} (y_n|f_1, var_1)^{a_n} \mathcal{N}	(y_n|f_2, var_2)^{1 - a_n}
+
+    where
+        Y represents the observed outputs Y = {y_n}_{n=1}^N,
+        F = {f_1, f_2}, f_k represents the GP for mode k,
+        var_k is the varince of mode k
+        here k=2
+    """
     def __init__(self,
                  variance_low=np.array([[0.005, 0.], [0., 0.005]]),
                  variance_high=np.array([[0.3, 0.], [0., 0.3]]),
                  invlink=inv_probit,
                  var_trainable=True,
                  **kwargs):
-        # def __init__(self, variance_low=np.array([0.005, 0.005]), variance_high=np.array([0.3, 0.3]), invlink=inv_probit, **kwargs):
+        """
+        Parameters
+        ----------
+        variance_low: noise covariance matrix 1
+        variance_high:
+        invlink: the inverse link function to be used, default is inverse probit
+        var_trainable: if False then variances not trainable, default True
+        """
         super().__init__(**kwargs)
         self.invlink = invlink
-        # transform = transforms.DiagMatrix(2)(transforms.positive)
-        # variance_low = Parameter(
-        #     variance_low, transform=transform, dtype=settings.float_type)
-        # variance_high = Parameter(
-        #     variance_high, transform=transform, dtype=settings.float_type)
         variance_low = Parameter(variance_low,
                                  transform=transforms.positive,
                                  dtype=settings.float_type)
@@ -59,16 +71,11 @@ class BernoulliGaussian(Likelihood):
 
     @params_as_tensors
     def predict_mean_and_var(self, Fmu, Fvar, idx):
-        # TODO: will this work in 1D?
-        # if tf.equal(tf.shape(Fmu)[1], tf.constant(1)):
-        # print(self.variances[0].shape)
-        if self.variances[0].shape[1] == 1:
+        if self.input_dim == 1:
             return tf.identity(Fmu), Fvar + tf.squeeze(self.variances[idx])
         else:
             return tf.identity(Fmu), Fvar + tf.diag_part(
                 tf.squeeze(self.variances[idx]))
-        # return tf.identity(Fmu), Fvar + tf.squeeze(self.variances[idx])
-        # return tf.identity(Fmu), Fvar + self.variances[idx]
 
     @params_as_tensors
     def predict_mean_and_var_a(self, Hmu, Hvar):

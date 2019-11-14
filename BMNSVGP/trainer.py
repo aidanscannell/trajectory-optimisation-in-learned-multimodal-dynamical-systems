21ce92b5350753d0705cf4b4ee66582a1e015481
Aidan Scannell <scannell.aidan@gmail.com>
2019-11-14 13:21:54 +0000

    Added saver to trainer.

--- BMNSVGP/trainer.py
+++ BMNSVGP/trainer.py
@@ -15,30 +15,12 @@
 import gpflow
 import numpy as np
 import tensorflow as tf
+
 from gen_data import func, func1, func2, gen_data
 from model import BMNSVGP
 from utils import plot_a, plot_contourf, plot_loss, plot_model, run_adam
 
 float_type = tf.float64
-
-# generate data set
-# input_dim = 2
-# output_dim = 2
-# if input_dim is 1 and output_dim is 1:
-#     func_list = [func]
-# elif input_dim is 2 and output_dim is 1:
-#     func_list = [func1]
-# elif input_dim is 2 and output_dim is 2:
-#     func_list = [func1, func2]
-# N = 900  # number of training observations
-
-# X, Y, a = gen_data(N,
-#                    input_dim,
-#                    output_dim,
-#                    low_lim=-2.,
-#                    high_lim=2.,
-#                    func_list=func_list,
-#                    plot_flag=False)
 
 
 def train_model(X, Y, num_iters=15000, vars_list=None, minibatch_size=100):
@@ -68,14 +50,33 @@ def train_model(X, Y, num_iters=15000, vars_list=None, minibatch_size=100):
 # plot_model_2d_uav(m, X, f=False, a=True, h=False, y=False, y_a=None, var=False)
 # plot_model_2d_uav(m, X, f=True , a=False, h=False, y=False, y_a=None, var=False)
 
-# saver = gpflow.saver.Saver()
-# saver.save('../saved_models/bmnsvgp', m)
-
 if __name__ == "__main__":
     data = np.load('../data/uav_data1.npz')
     X = data['x']
     Y = data['y']
 
+    # generate data set
+    # input_dim = 2
+    # output_dim = 2
+    # if input_dim is 1 and output_dim is 1:
+    #     func_list = [func]
+    # elif input_dim is 2 and output_dim is 1:
+    #     func_list = [func1]
+    # elif input_dim is 2 and output_dim is 2:
+    #     func_list = [func1, func2]
+    # N = 900  # number of training observations
+
+    # X, Y, a = gen_data(N,
+    #                    input_dim,
+    #                    output_dim,
+    #                    low_lim=-2.,
+    #                    high_lim=2.,
+    #                    func_list=func_list,
+    #                    plot_flag=False)
+
     m, logger = train_model(X, Y)
 
     plot_loss(logger)
+
+    # saver = gpflow.saver.Saver()
+    # saver.save('./saved_models/fake_alpha', m)

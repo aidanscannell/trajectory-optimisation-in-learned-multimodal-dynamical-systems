e96b48e5f6cc5f401a9c3dfbfade0c0178fd44b0
Aidan Scannell <scannell.aidan@gmail.com>
2020-01-22 17:31:10 +0000

    commit before using scipy

--- BMNSVGP/probabilistic_geodesic.py
+++ BMNSVGP/probabilistic_geodesic.py
@@ -1,7 +1,5 @@
-# import gpflow
-# from gpflow.kernels import RBF
+import sys
 
-# kernel = gpflow.kernels.RBF(variance=2.)
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -9,29 +7,22 @@ import tensorflow_probability as tfp
 tfd = tfp.distributions
 tfk = tfp.math.psd_kernels
 
+# Load kernel hyper-params and create tf kernel
 params = np.load('saved_models/params.npz')
-# l = params['l'].reshape(2, 1)
 l = params['l']  # [2]
-a = params['a']  # [1]
-x = params['x']  # [100 x 2]
-
-num_data = x.shape[0]
-input_dim = x.shape[1]
-output_dim = 1
-
-Y = tf.constant(np.ones([num_data, output_dim]), dtype=tf.float64)
-
-amplitude = tf.constant([a], dtype=tf.float64)
+a = params['var']  # [1]
+x = params['x']  # [num_data x 2]
+y = params['a']  # [num_data x 2] meen and var of alpha
+y = y[0:1, :, 0]
+x = tf.constant(x, dtype=tf.float64)
+Y = tf.constant(y.T, dtype=tf.float64)
+amplitude = tf.constant(a, dtype=tf.float64)
 length_scale = tf.constant(l, dtype=tf.float64)
 kernel = tfk.FeatureScaled(
     tfk.ExponentiatedQuadratic(amplitude=amplitude, length_scale=1.),
     length_scale)
-# kernel = tfk.FeatureScaled(
-#     tfk.ExponentiatedQuadratic(amplitude=amplitude, length_scale=length_scale),
-#     length_scale)
 
-x = tf.constant(x, dtype=tf.float64)
-s = tf.constant(np.ones([1, input_dim]), dtype=tf.float64)
+num_data, input_dim, output_dim = x.shape[0], x.shape[1], 1
 
 Kxx = kernel.matrix(x, x)
 jitter = 1e-8
@@ -40,79 +31,104 @@ diagonal = tf.linalg.diag(
 Kxx = Kxx + jitter
 iKxx = tf.linalg.inv(Kxx)
 
-with tf.GradientTape(persistent=True) as g:
-    g.watch(s)
-    with tf.GradientTape(persistent=True) as t:
-        t.watch(s)
-        Kxs = kernel.matrix(x, s)
-        with tf.GradientTape() as tt:
-            tt.watch(s)
-            Kss = kernel.matrix(s, s)
-        dk_dss = tt.jacobian(Kss, s)
 
-    dk_ds = t.jacobian(Kxs, s)
-    dk_ds = tf.squeeze(dk_ds)
+def ode_fn(t, y):
+    c = y[0]
+    g = y[1]
+    assert c.shape == [1, input_dim]
+    assert g.shape == [1, input_dim]
 
-    d2k_d2s = t.jacobian(dk_dss, s)
+    with tf.GradientTape(persistent=True) as grad:
+        grad.watch(c)
+        with tf.GradientTape(persistent=True) as t:
+            t.watch(c)
+            Kxs = kernel.matrix(x, c)
+            with tf.GradientTape() as tt:
+                tt.watch(c)
+                Kss = kernel.matrix(c, c)
+            dk_dss = tt.jacobian(Kss, c)
 
-    d2k_d2s = tf.squeeze(d2k_d2s)
+        dk_ds = t.jacobian(Kxs, c)
+        dk_ds = tf.squeeze(dk_ds)
 
-    # calculate mean and variance of J
-    dk_dsT = tf.transpose(dk_ds)
-    mu_j = dk_dsT @ iKxx @ Y  # [input_dim x 1]
-    assert mu_j.shape == [1, input_dim, 1]
-    cov_j = d2k_d2s - dk_dsT @ iKxx @ dk_ds  # [input_dim x inut_dim]
-    assert cov_j.shape == [1, input_dim, input_dim]
+        d2k_d2s = t.jacobian(dk_dss, c)
 
-    mu_jT = tf.linalg.matrix_transpose(mu_j)
-    assert mu_jT.shape == [1, 1, input_dim]
+        d2k_d2s = tf.squeeze(d2k_d2s)
 
-    jTj = mu_jT @ mu_j  # [1 x 1]
-    assert jTj.shape == [1, 1, 1]
+        # calculate mean and variance of J
+        dk_dsT = tf.transpose(dk_ds)  # [input_dim x num_data]
+        mu_j = dk_dsT @ iKxx @ Y  # [input_dim x 1]
+        assert mu_j.shape == [input_dim, 1]
+        # assert mu_j.shape == [1, input_dim, 1]
+        cov_j = d2k_d2s - dk_dsT @ iKxx @ dk_ds  # [input_dim x inut_dim]
+        assert cov_j.shape == [input_dim, input_dim]
+        # assert cov_j.shape == [1, input_dim, input_dim]
 
-    G = jTj + output_dim * cov_j  # [input_dim x input_dim]
-    assert G.shape == [1, input_dim, input_dim]
-    vecG = tf.reshape(G, [-1])  # stack columns of G
-    assert vecG.shape == [input_dim * input_dim]
+        mu_jT = tf.linalg.matrix_transpose(mu_j)
+        assert mu_jT.shape == [1, input_dim]
+        # assert mu_jT.shape == [1, 1, input_dim]
 
-dGds = g.jacobian(vecG, s)
-assert dGds.shape == [input_dim * input_dim, 1, input_dim]
-dGds = tf.squeeze(dGds)
-assert dGds.shape == [input_dim * input_dim, input_dim]
+        # TODO make this DxD
+        jTj = mu_j @ mu_jT  # [D x D]
+        # jTj = mu_jT @ mu_j  # [1 x 1]
+        # assert jTj.shape == [1, 1, 1]
+        assert jTj.shape == [input_dim, input_dim]
 
-# vecG = tf.stack([G[0, :], G[1, :]])  # [input_dim^2 x input_dim]
-# print(vecG.shape)
+        G = jTj + output_dim * cov_j  # [input_dim x input_dim]
+        assert G.shape == [input_dim, input_dim]
+        # assert G.shape == [1, input_dim, input_dim]
+        vecG = tf.reshape(G, [-1])  # stack columns of G
+        assert vecG.shape == [input_dim * input_dim]
 
-# with tf.GradientTape() as t:
-#     t.watch(s)
-#     Kxs = kernel.matrix(x, s)
-# dk_ds = t.jacobian(Kxs, s)
-# dk_ds = tf.squeeze(dk_ds)
+    dGds = grad.jacobian(vecG, c)
+    assert dGds.shape == [input_dim * input_dim, 1, input_dim]
+    dGds = tf.squeeze(dGds)
+    assert dGds.shape == [input_dim * input_dim, input_dim]
+    dGdsT = tf.transpose(dGds)
+    assert dGdsT.shape == [input_dim, input_dim * input_dim]
 
-# with tf.GradientTape() as t:
-#     t.watch(s)
-#     with tf.GradientTape() as tt:
-#         tt.watch(s)
-#         Kss = kernel.matrix(s, s)
-#     dk_dss = tt.jacobian(Kss, s)
-# d2k_d2s = t.jacobian(dk_dss, s)
+    invG = tf.linalg.inv(G)
 
-# d2k_d2s = tf.squeeze(d2k_d2s)
+    gT = tf.transpose(g)
+    assert gT.shape == [input_dim, 1]
+    operator_gT = tf.linalg.LinearOperatorFullMatrix(gT)
+    CkronC = tf.linalg.LinearOperatorKronecker([operator_gT, operator_gT])
+    assert CkronC.shape == [input_dim * input_dim, 1]
+    dg = -0.5 * invG @ dGdsT @ CkronC.to_dense()
+    dgT = tf.transpose(dg)
+    assert dgT.shape == [1, input_dim]
+    dc = g
+    assert dc.shape == [1, input_dim]
+    y_out = [dc, dgT]
+    # return y_out
+    return [dc, dgT]
 
-# # calculate mean and variance of J
-# iKxx = tf.linalg.inv(Kxx)
-# dk_dsT = tf.transpose(dk_ds)
-# mu_j = dk_dsT @ iKxx @ Y  # [input_dim x 1]
-# cov_j = d2k_d2s - dk_dsT @ iKxx @ dk_ds  # [input_dim x inut_dim]
 
-# mu_jT = tf.transpose(mu_j)
-# jTj = mu_jT @ mu_j  # [1 x 1]
+# t_init, t0, t1 = 0., 0.5, 1.
+# t_init = tf.constant([0., 0.], dtype=tf.float64)
+t_init = 0.
+t0 = 0.1
+t1 = 1.
+# solution_times = tf.constant(np.linspace(t0, t1, 3), dtype=tf.float64)
+# solution_times = [0.4, 0.8, 1.]
+solution_times = [0.1, 0.2]
+# y_init = tf.constant([1., 1.], dtype=tf.float64)
 
-# with tf.GradientTape() as t:
-#     t.watch(s)
-#     G = jTj + output_dim * cov_j  # [input_dim x input_dim]
+s = tf.constant(np.ones([1, input_dim]), dtype=tf.float64)
+# dg = ode_fn(s, [s, s])
 
-# dGds = t.gradient(G, s)
-# print(dGds)
-# # vecG = tf.stack([G[0, :], G[1, :]])  # [input_dim^2 x input_dim]
-# # print(vecG.shape)
+c = tf.constant(np.ones([1, input_dim]), dtype=tf.float64) * 0.
+g = tf.constant(np.ones([1, input_dim]), dtype=tf.float64) * 0.1
+y_init = [c, g]
+results = tfp.math.ode.BDF().solve(ode_fn,
+                                   t_init,
+                                   y_init,
+                                   solution_times=solution_times)
+y0 = results.states[0]  # == dot(matrix_exp(A * t0), y_init)
+y1 = results.states[1]  # == dot(matrix_exp(A * t1), y_init)
+with tf.Session() as sess:
+    # results = sess.run(dg)
+    results = sess.run(results)
+    print(results.states)
+    # print(y0)
+    # print(y1)

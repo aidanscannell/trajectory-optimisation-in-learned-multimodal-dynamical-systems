272789d66c279a02d635e186ee908092a71124e0
Aidan Scannell <scannell.aidan@gmail.com>
2020-03-10 11:38:43 +0000

    updated tf.erf to tf.math.erf

--- BMNSVGP/likelihood.py
+++ BMNSVGP/likelihood.py
@@ -22,7 +22,8 @@ from gpflow.params import Parameter, ParamList
 
 def inv_probit(x):
     jitter = 1e-3  # ensures output is strictly between 0 and 1
-    return 0.5 * (1.0 + tf.erf(x / np.sqrt(2.0))) * (1 - 2 * jitter) + jitter
+    return 0.5 * (1.0 + tf.math.erf(x / np.sqrt(2.0))) * (1 -
+                                                          2 * jitter) + jitter
 
 
 class BernoulliGaussian(Likelihood):

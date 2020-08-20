562171f15944a4e5a88462f426b2abb0963279f4
Aidan Scannell <scannell.aidan@gmail.com>
2020-08-20 10:31:46 +0100

    Change DiffRBF import to kernels.py.
    Changed return a_mu to Y and deleted a_var

--- ProbGeo/utils/gp.py
+++ ProbGeo/utils/gp.py
@@ -1,6 +1,6 @@
 from jax import numpy as np
 from jax import scipy as sp
-from ProbGeo.derivative_kernel_gpy import DiffRBF
+from ProbGeo.kernels import DiffRBF
 
 
 def load_data_and_init_kernel_fake(filename):
@@ -9,10 +9,10 @@ def load_data_and_init_kernel_fake(filename):
     l = params['l']  # [2]
     var = params['var']  # [1]
     X = params['x']  # [num_data x 2]
-    a_mu = params['a_mu']  # [num_data x 1] mean of alpha
-    a_var = params['a_var']  # [num_data x 1] variance of alpha
+    Y = params['a_mu']  # [num_data x 1] mean of alpha
+    # a_var = params['a_var']  # [num_data x 1] variance of alpha
     kernel = DiffRBF(X.shape[1], variance=var, lengthscale=l, ARD=True)
-    return X, a_mu, a_var, kernel
+    return X, Y, kernel
 
 
 def load_data_and_init_kernel_sparse(filename):

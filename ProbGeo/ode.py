b6f8179888f4cb6aeee7aaafd3a861aed05d6554
Aidan Scannell <scannell.aidan@gmail.com>
2020-08-21 11:47:19 +0100

    changed metric_fn_args to metric_fn_kwargs

--- ProbGeo/ode.py
+++ ProbGeo/ode.py
@@ -9,7 +9,7 @@ from ProbGeo.metric_tensor import calc_vec_metric_tensor
 
 
 @partial(jit, static_argnums=(2, 3))
-def geodesic_ode(t, state, metric_fn, metric_fn_args):
+def geodesic_ode(t, state, metric_fn, metric_fn_kwargs):
     input_dim = int(state.shape[0] / 2)
     pos = state[:input_dim].reshape(1, -1)
     vel = state[input_dim:].reshape(1, -1)
@@ -18,14 +18,14 @@ def geodesic_ode(t, state, metric_fn, metric_fn_args):
     # TODO implement value and jac
     grad_func = jacfwd(calc_vec_metric_tensor, 0)
     grad_vec_metric_tensor_wrt_pos = grad_func(pos.reshape(-1), metric_fn,
-                                               metric_fn_args)
+                                               metric_fn_kwargs)
     # grad_vec_metric_tensor_wrt_pos = grad_vec_metric_tensor_wrt_pos.reshape(
     #     input_dim, input_dim * input_dim)
     print('grad vec')
     print(grad_vec_metric_tensor_wrt_pos.shape)
     grad_vec_metric_tensor_wrt_posT = grad_vec_metric_tensor_wrt_pos.T
 
-    metric_tensor, _, _ = metric_fn(pos, *metric_fn_args)
+    metric_tensor, _, _ = metric_fn(pos, **metric_fn_kwargs)
     # TODO implement cholesky if metric_tensor is PSD
     inv_metric_tensor = np.linalg.inv(metric_tensor)
 
@@ -37,9 +37,9 @@ def geodesic_ode(t, state, metric_fn, metric_fn_args):
 
 
 @partial(jit, static_argnums=(2, 3))
-def geodesic_ode_bvp(t, state, metric_fn, metric_fn_args):
+def geodesic_ode_bvp(t, state, metric_fn, metric_fn_kwargs):
     state_prime = vmap(geodesic_ode,
                        in_axes=(None, 1, None, None))(t, state, metric_fn,
-                                                      metric_fn_args)
+                                                      metric_fn_kwargs)
     state_primeT = state_prime.T
     return state_primeT

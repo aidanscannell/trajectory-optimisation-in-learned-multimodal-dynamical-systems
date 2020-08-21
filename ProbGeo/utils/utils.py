f08c969e56fb980d27856b1c10c8d7af662ed735
Aidan Scannell <scannell.aidan@gmail.com>
2020-08-21 11:46:03 +0100

    added basic funcs for training gpflow svgp

--- ProbGeo/utils/utils.py
+++ ProbGeo/utils/utils.py
@@ -1,1 +1,37 @@
+import gpflow
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
+
+
+class Logger(gpflow.actions.Action):
+    def __init__(self, model):
+        self.model = model
+        self.logf = []
+
+    def run(self, ctx):
+        if (ctx.iteration % 10) == 0:
+            # Extract likelihood tensor from Tensorflow session
+            likelihood = -ctx.session.run(self.model.likelihood_tensor)
+            # Append likelihood value to list
+            self.logf.append(likelihood)
+
+
+def run_adam(model, maxiter=450):
+    # Create an Adam Optimiser action
+    adam = gpflow.train.AdamOptimizer().make_optimize_action(model)
+    # Create a Logger action
+    logger = Logger(model)
+    actions = [adam, logger]
+    # Create optimisation loop that interleaves Adam with Logger
+    loop = gpflow.actions.Loop(actions, stop=maxiter)()
+    # Bind current TF session to model
+    model.anchor(model.enquire_session())
+    return logger
+
+
+def plot_loss(logger):
+    plt.plot(-np.array(logger.logf))
+    plt.xlabel('iteration (x10)')
+    plt.ylabel('ELBO')
+    plt.show(block=True)

4ed8c81608ede374e6b68d753899d58dee5cabe4
Aidan Scannell <scannell.aidan@gmail.com>
2020-09-16 16:22:29 +0100

    added try/except to handle diff numbers of return args from metric_fn

--- ProbGeo/ode.py
+++ ProbGeo/ode.py
@@ -6,6 +6,8 @@ from ProbGeo.metric_tensor import calc_vec_metric_tensor
 
 @partial(jit, static_argnums=(2, 3))
 def geodesic_ode(t, state, metric_fn, metric_fn_kwargs):
+    print('inside geodesic_ode')
+    print(state.shape)
     input_dim = int(state.shape[0] / 2)
     pos = state[:input_dim].reshape(1, -1)
     vel = state[input_dim:].reshape(1, -1)
@@ -21,15 +23,27 @@ def geodesic_ode(t, state, metric_fn, metric_fn_kwargs):
     print(grad_vec_metric_tensor_wrt_pos.shape)
     grad_vec_metric_tensor_wrt_posT = grad_vec_metric_tensor_wrt_pos.T
 
-    metric_tensor, _, _ = metric_fn(pos, **metric_fn_kwargs)
+    try:
+        metric_tensor, _ = metric_fn(pos, **metric_fn_kwargs)
+    except:
+        metric_tensor, _, _ = metric_fn(pos, **metric_fn_kwargs)
     # TODO implement cholesky if metric_tensor is PSD
+    jitter = 1e-6
+    metric_tensor += np.eye(input_dim) * jitter
     inv_metric_tensor = np.linalg.inv(metric_tensor)
+    print('inv')
+    print(inv_metric_tensor)
+    # assert np.isnan(inv_metric_tensor).any()
 
     acc = -0.5 * inv_metric_tensor @ grad_vec_metric_tensor_wrt_posT @ kron_vel
     acc = acc.reshape(1, input_dim)
 
     state_prime = np.concatenate([vel, acc], -1)
-    return state_prime.reshape(-1)
+    print('state_prime')
+    print(state_prime)
+    # return state_prime
+    return state_prime.reshape([-1])
+    # return state_prime.reshape(-1)
 
 
 @partial(jit, static_argnums=(2, 3))

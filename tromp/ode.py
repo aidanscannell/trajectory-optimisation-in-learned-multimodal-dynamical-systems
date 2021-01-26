a0b35df7425a16588a0ec4431672b218cac53720
Aidan Scannell <scannell.aidan@gmail.com>
2021-01-26 18:36:21 +0000

    Changed ode to a class

--- tromp/ode.py
+++ tromp/ode.py
@@ -1,59 +1,171 @@
-from jax import jacfwd, jit
-from jax import numpy as np
-from jax import partial, vmap
+import abc
 
-from tromp.metric_tensor import calc_vec_metric_tensor
+# from jax import jacfwd, jit, partial, vmap
+import jax
+import jax.numpy as jnp
+import objax
+from gpjax.utilities import leading_transpose
 
+from tromp.metric_tensors import RiemannianMetricTensor
 
-# @partial(jit, static_argnums=(2))
-# @partial(jit, static_argnums=(2, 3))
-@partial(jit, static_argnums=(2, 3))
-def geodesic_ode(t, state, metric_fn, metric_fn_kwargs):
-    print("inside geodesic_ode")
-    print(state.shape)
-    input_dim = int(state.shape[0] / 2)
-    pos = state[:input_dim].reshape(1, -1)
-    vel = state[input_dim:].reshape(1, -1)
-    kron_vel = np.kron(vel, vel).T
-
-    # TODO implement value and jac
-    grad_func = jacfwd(calc_vec_metric_tensor, 0)
-    grad_vec_metric_tensor_wrt_pos = grad_func(
-        pos.reshape(-1), metric_fn, metric_fn_kwargs
-    )
-    # grad_vec_metric_tensor_wrt_pos = grad_vec_metric_tensor_wrt_pos.reshape(
-    #     input_dim, input_dim * input_dim)
-    print("grad vec")
-    print(grad_vec_metric_tensor_wrt_pos.shape)
-    grad_vec_metric_tensor_wrt_posT = grad_vec_metric_tensor_wrt_pos.T
-
-    try:
-        metric_tensor, _ = metric_fn(pos, **metric_fn_kwargs)
-    except:
-        metric_tensor, _, _ = metric_fn(pos, **metric_fn_kwargs)
-    # TODO implement cholesky if metric_tensor is PSD
-    jitter = 1e-6
-    metric_tensor += np.eye(input_dim) * jitter
-    inv_metric_tensor = np.linalg.inv(metric_tensor)
-    print("inv")
-    print(inv_metric_tensor)
-    # assert np.isnan(inv_metric_tensor).any()
-
-    acc = -0.5 * inv_metric_tensor @ grad_vec_metric_tensor_wrt_posT @ kron_vel
-    acc = acc.reshape(1, input_dim)
-
-    state_prime = np.concatenate([vel, acc], -1)
-    print("state_prime")
-    print(state_prime)
-    # return state_prime
-    return state_prime.reshape([-1])
-    # return state_prime.reshape(-1)
+States = jnp.ndarray
 
 
-@partial(jit, static_argnums=(2, 3))
-def geodesic_ode_bvp(t, state, metric_fn, metric_fn_kwargs):
-    state_prime = vmap(geodesic_ode, in_axes=(None, 1, None, None))(
-        t, state, metric_fn, metric_fn_kwargs
-    )
-    state_primeT = state_prime.T
-    return state_primeT
+class ODE(objax.Module, abc.ABC):
+    @abc.abstractmethod
+    def ode_fn(self, times: jnp.ndarray, states: States) -> States:
+        raise NotImplementedError
+
+
+class GeodesicODE(ODE):
+    def __init__(
+        self, metric_tensor: RiemannianMetricTensor, jitter: jnp.float64 = 1e-6
+    ):
+        self.metric_tensor = metric_tensor
+
+    def metric_fn(self, pos_guesses, full_cov: bool = True):
+        # return self.metric_tensor(pos_guesses, full_cov=full_cov)
+        return self.metric_tensor.metric_fn(pos_guesses, full_cov=full_cov)
+
+    def ode_fn(self, times, states):
+        print("inside ode_fn")
+        print(states.shape)
+        jitter = 1e-6
+        full_cov = True
+        input_dim = int(states.shape[-1] / 2)
+        if len(states.shape) == 1:
+            num_states = 1
+            # pos = states[:input_dim].reshape(1, input_dim)
+            # vel = states[input_dim:].reshape(1, input_dim)
+            pos = states[:input_dim].reshape(input_dim)
+            vel = states[input_dim:].reshape(input_dim)
+            kron_vel = jnp.kron(vel, vel).reshape(input_dim * 2, 1)
+        elif len(states.shape) == 2:
+            num_states = states.shape[0]
+            pos = states[:, :input_dim].reshape(-1, input_dim)
+            vel = states[:, input_dim:].reshape(-1, input_dim)
+            kron_vel = jax.vmap(jnp.kron)(vel, vel)
+            kron_vel = kron_vel[..., jnp.newaxis]
+        print("pos")
+        print(pos.shape)
+        print(vel.shape)
+        print("kron_vel")
+        print(kron_vel.shape)
+
+        # TODO implement value and jac
+        grad_vec_metric_tensor_wrt_pos = (
+            self.metric_tensor.grad_vec_metric_tensor_wrt_Xnew(
+                Xnew=pos, full_cov=full_cov
+            )
+        )
+        # grad_vec_metric_tensor_wrt_pos = grad_vec_metric_tensor_wrt_pos.reshape(
+        #     input_dim, input_dim * input_dim)
+        print("grad vec")
+        print(grad_vec_metric_tensor_wrt_pos.shape)
+        grad_vec_metric_tensor_wrt_posT = leading_transpose(
+            grad_vec_metric_tensor_wrt_pos, [..., -1, -2]
+        )
+        print(grad_vec_metric_tensor_wrt_posT.shape)
+
+        metric_tensor = self.metric_tensor(pos, full_cov)
+        # TODO implement cholesky if metric_tensor is PSD
+        metric_tensor = metric_tensor + jnp.eye(input_dim) * jitter
+        inv_metric_tensor = jnp.linalg.inv(metric_tensor)
+        print("inv")
+        print(inv_metric_tensor)
+
+        acc = (
+            -0.5
+            * inv_metric_tensor
+            @ grad_vec_metric_tensor_wrt_posT
+            @ kron_vel
+        )
+        print("acc")
+        print(acc.shape)
+        acc = acc.reshape(num_states, input_dim)
+        if len(states.shape) == 1:
+            vel = vel.reshape(1, input_dim)
+        # elif len(states.shape) == 2:
+        #     acc = acc.reshape(num_states, input_dim)
+        # print(acc.shape)
+
+        states_prime = jnp.concatenate([vel, acc], -1)
+        print("states_prime")
+        print(states_prime)
+
+        # return flattended states_prime if states was flat
+        if num_states == 1:
+            return states_prime.reshape([-1])
+        else:
+            return states_prime
+
+        # return state_prime
+        # return state_prime.reshape([-1])
+        # return state_prime.reshape(-1)
+
+
+# class GeodesicODE(ODE):
+#     def __init__(self, metric_fn, metric_fn_kwargs):
+#         self.metric_fn = metric_fn
+#         self.metric_fn_kwargs = metric_fn_kwargs
+
+#     def ode_fn(self, time, state):
+#         print("inside ode_fn")
+#         print(state.shape)
+#         input_dim = int(state.shape[0] / 2)
+#         pos = state[:input_dim].reshape(1, -1)
+#         vel = state[input_dim:].reshape(1, -1)
+#         kron_vel = jnp.kron(vel, vel).T
+
+#         # TODO implement value and jac
+#         grad_func = jax.jacfwd(calc_vec_metric_tensor, 0)
+#         grad_vec_metric_tensor_wrt_pos = grad_func(
+#             pos.reshape(-1), self.metric_fn, self.metric_fn_kwargs
+#         )
+#         # grad_vec_metric_tensor_wrt_pos = grad_vec_metric_tensor_wrt_pos.reshape(
+#         #     input_dim, input_dim * input_dim)
+#         print("grad vec")
+#         print(grad_vec_metric_tensor_wrt_pos.shape)
+#         grad_vec_metric_tensor_wrt_posT = grad_vec_metric_tensor_wrt_pos.T
+
+#         try:
+#             metric_tensor, _ = self.metric_fn(pos, **self.metric_fn_kwargs)
+#         except:
+#             metric_tensor, _, _ = self.metric_fn(pos, **self.metric_fn_kwargs)
+#         # TODO implement cholesky if metric_tensor is PSD
+#         jitter = 1e-6
+#         metric_tensor += jnp.eye(input_dim) * jitter
+#         inv_metric_tensor = jnp.linalg.inv(metric_tensor)
+#         print("inv")
+#         print(inv_metric_tensor)
+#         # assert jnp.isnan(inv_metric_tensor).any()
+
+#         acc = (
+#             -0.5
+#             * inv_metric_tensor
+#             @ grad_vec_metric_tensor_wrt_posT
+#             @ kron_vel
+#         )
+#         acc = acc.reshape(1, input_dim)
+
+#         state_prime = jnp.concatenate([vel, acc], -1)
+#         print("state_prime")
+#         print(state_prime)
+#         # return state_prime
+#         return state_prime.reshape([-1])
+
+#     @jax.partial(jax.jit, static_argnums=(1, 2))
+#     def calc_vec_metric_tensor(pos, metric_fn, metric_fn_kwargs):
+#         print("here calc vec metric tensor")
+#         print(pos.shape)
+#         pos = pos.reshape(1, -1)
+#         try:
+#             metric_tensor, _ = metric_fn(pos, **metric_fn_kwargs)
+#             # TODO add the correct exception
+#         except:
+#             metric_tensor, _, _ = metric_fn(pos, **metric_fn_kwargs)
+#         input_dim = pos.shape[1]
+#         vec_metric_tensor = metric_tensor.reshape(
+#             input_dim * input_dim,
+#         )
+#         return vec_metric_tensor

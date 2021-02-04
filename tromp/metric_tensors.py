8b4d94f46c47e67bc72283bae09c923346c5fbce
Aidan Scannell <scannell.aidan@gmail.com>
2021-02-04 09:54:59 +0000

    Moved classes to top and deleted old methods

--- tromp/metric_tensors.py
+++ tromp/metric_tensors.py
@@ -5,11 +5,177 @@ import jax.numpy as jnp
 import objax
 from gpjax.custom_types import InputData
 # from gpjax.kernels import Kernel
-from gpjax.models.gp import SVGP, GPBase
-from gpjax.prediction import gp_jacobian
+from gpjax.models import SVGP, GPBase
+# from gpjax.prediction import gp_jacobian
 from gpjax.utilities import leading_transpose
 
 MetricTensor = jnp.ndarray
+
+
+class MetricTensorBase(objax.Module):
+    def metric_fn(self, Xnew: InputData) -> MetricTensor:
+        raise NotImplementedError
+
+    def __call__(self, Xnew: InputData) -> MetricTensor:
+        return self.metric_fn(Xnew)
+
+
+class RiemannianMetricTensor(MetricTensorBase):
+    def metric_fn(self, Xnew) -> MetricTensor:
+        raise NotImplementedError
+
+
+class SVGPMetricTensor(RiemannianMetricTensor):
+    def __init__(
+        self,
+        gp: SVGP,
+        covariance_weight: jnp.float64 = 1.0,
+        jitter: jnp.float64 = 1.0e-4,
+    ):
+        self.gp = gp
+        self.covariance_weight = covariance_weight
+        self.jitter = jitter
+
+    def __call__(self, Xnew: InputData, full_cov: bool = True):
+        return self.metric_fn(Xnew, full_cov=full_cov)
+
+    def grad_vec_metric_tensor_wrt_Xnew(
+        self, Xnew: InputData, full_cov: bool = True
+    ):
+        def calc_vec_metric_tensor(Xnew):
+            # pos = pos.reshape(1, -1)
+            print("inside vec metric tensor Xnew")
+            print(Xnew.shape)
+            metric_tensor = self.metric_fn(Xnew, full_cov=full_cov)
+            # if full_cov = False:
+            input_dim = Xnew.shape[-1]
+            vec_metric_tensor = metric_tensor.reshape(
+                input_dim * input_dim,
+            )
+            return vec_metric_tensor
+
+        grad_func = jax.jacfwd(calc_vec_metric_tensor, 0)
+        if len(Xnew.shape) == 1:
+            grad_vec_metric_tensor_wrt_Xnew = grad_func(Xnew)
+        else:
+            grad_vec_metric_tensor_wrt_Xnew = jax.vmap(grad_func)(Xnew)
+        return grad_vec_metric_tensor_wrt_Xnew
+
+    # def calc_vec_metric_tensor(self, Xnew, full_cov: bool = True):
+    #     # pos = pos.reshape(1, -1)
+    #     print("inside vec metric tensor Xnew")
+    #     print(Xnew.shape)
+    #     metric_tensor = self.metric_fn(Xnew, full_cov=full_cov)
+    #     input_dim = Xnew.shape[-1]
+    #     vec_metric_tensor = metric_tensor.reshape(
+    #         input_dim * input_dim,
+    #     )
+    #     return vec_metric_tensor
+
+    def metric_fn(
+        self,
+        Xnew: InputData,
+        full_cov: bool = True,
+    ) -> MetricTensor:
+        # jac_mean, jac_cov = gp_jacobian(
+        #     Xnew,
+        #     self.gp.inducing_variable,
+        #     self.gp.kernel,
+        #     self.gp.mean_function,
+        #     f=self.gp.q_mu,
+        #     full_cov=full_cov,
+        #     q_sqrt=self.gp.q_sqrt,
+        #     jitter=self.jitter,
+        #     white=self.gp.whiten,
+        # )
+        print("inside metric_fn what is Xnew.shape")
+        print(Xnew.shape)
+        input_dim = Xnew.shape[-1]
+        if len(Xnew.shape) == 1:
+            # Xnew = Xnew.reshape(1, -1)
+            num_data = 1
+        else:
+            num_data = Xnew.shape[0]
+        print(Xnew.shape)
+
+        print("before gp.predict_jac what is Xnew.shape")
+        print(Xnew.shape)
+        jac_mean, jac_cov = self.gp.predict_jacobian_f_wrt_Xnew(
+            Xnew, full_cov=full_cov
+        )
+        # TODO this is only true if full_cov=True
+        # assert jac_mean.shape == (num_data, input_dim, 1)
+        # assert jac_cov.shape == (num_data, input_dim, input_dim)
+
+        jac_mean_trans = leading_transpose(jac_mean, [..., -1, -2])
+        expected_jac_outer_prod = jnp.matmul(
+            jac_mean, jac_mean_trans
+        )  # [input_dim x input_dim]
+        print("expected_jac_outer_prod")
+        print(expected_jac_outer_prod.shape)
+
+        # jax.experimental.host_callback.id_print(expected_jac_outer_prod)
+        # expected_metric_tensor = expected_jac_outer_prod + cov_weight * output_dim * cov_j
+        expected_metric_tensor = (
+            expected_jac_outer_prod + self.covariance_weight * jac_cov
+        )
+        print("expected_metric_tensor")
+        print(expected_metric_tensor.shape)
+        # expected_metric_tensor = cov_weight * cov_j.T
+        # expected_metric_tensor = cov_j
+        # assert expected_metric_tensor.shape == (num_data, input_dim, input_dim)
+        return expected_metric_tensor
+
+    # def metric_fn(
+    #     self,
+    #     Xnew: InputData,
+    #     full_cov: bool = True,
+    # )-> MetricTensor:
+    #     def calc_expected_metric_tensor_single(x):
+    #         if len(x.shape) == 1:
+    #             x = x.reshape(1, input_dim)
+
+    #         mu_j, cov_j = gp_jacobian(
+    #             x,
+    #             self.gp.inducing_variable,
+    #             self.gp.kernel,
+    #             self.gp.mean_function,
+    #             f=self.gp.q_mu,
+    #             full_cov=full_cov,
+    #             # full_cov=False,
+    #             q_sqrt=self.gp.q_sqrt,
+    #             jitter=self.jitter,
+    #             white=self.gp.whiten,
+    #         )
+    #         # mu_j, cov_j = gp_jacobian_hard_coded(cov_fn, x, X, Y, jitter=jitter)
+    #         assert mu_j.shape == (input_dim, 1)
+    #         assert cov_j.shape == (input_dim, input_dim)
+
+    #         expected_jac_outer_prod = jnp.matmul(
+    #             mu_j, mu_j.T
+    #         )  # [input_dim x input_dim]
+    #         assert expected_jac_outer_prod.shape == (input_dim, input_dim)
+
+    #         # expected_metric_tensor = expected_jac_outer_prod + cov_weight * output_dim * cov_j
+    #         expected_metric_tensor = (
+    #             expected_jac_outer_prod + self.covariance_weight * cov_j
+    #         )
+    #         # expected_metric_tensor = cov_weight * cov_j.T
+    #         # expected_metric_tensor = cov_j
+    #         assert expected_metric_tensor.shape == (input_dim, input_dim)
+    #         return expected_metric_tensor, mu_j, cov_j
+
+    #     f = self.gp.q_mu
+    #     num_test_inputs = Xnew.shape[0]
+    #     input_dim = Xnew.shape[1]
+    #     output_dim = f.shape[1]
+
+    #     expected_metric_tensor, mu_j, cov_j = jax.vmap(
+    #         calc_expected_metric_tensor_single, in_axes=(0)
+    #     )(Xnew)
+    #     # return expected_metric_tensor, mu_j, cov_j
+    #     return expected_metric_tensor
+
 
 # @jax.partial(jax.jit, static_argnums=(1, 2))
 def metric_tensor_fn(Xnew, fun, fun_kwargs):
@@ -135,280 +301,3 @@ def metric_tensor_fn(Xnew, fun, fun_kwargs):
 #         input_dim * input_dim,
 #     )
 #     return vec_metric_tensor
-
-
-class MetricTensorBase(objax.Module):
-    def metric_fn(self, Xnew: InputData) -> MetricTensor:
-        raise NotImplementedError
-
-    def __call__(self, Xnew: InputData) -> MetricTensor:
-        return self.metric_fn(Xnew)
-
-
-class RiemannianMetricTensor(MetricTensorBase):
-    def metric_fn(self, Xnew) -> MetricTensor:
-        raise NotImplementedError
-
-
-class SVGPMetricTensor(RiemannianMetricTensor):
-    def __init__(
-        self,
-        gp: SVGP,
-        covariance_weight: jnp.float64 = 1.0,
-        jitter: jnp.float64 = 1.0e-4,
-    ):
-        self.gp = gp
-        self.covariance_weight = covariance_weight
-        self.jitter = jitter
-
-    def __call__(self, Xnew: InputData, full_cov: bool = True):
-        return self.metric_fn(Xnew, full_cov=full_cov)
-
-    def grad_vec_metric_tensor_wrt_Xnew(
-        self, Xnew: InputData, full_cov: bool = True
-    ):
-        def calc_vec_metric_tensor(Xnew):
-            # pos = pos.reshape(1, -1)
-            print("inside vec metric tensor Xnew")
-            print(Xnew.shape)
-            metric_tensor = self.metric_fn(Xnew, full_cov=full_cov)
-            # if full_cov = False:
-            input_dim = Xnew.shape[-1]
-            vec_metric_tensor = metric_tensor.reshape(
-                input_dim * input_dim,
-            )
-            return vec_metric_tensor
-
-        grad_func = jax.jacfwd(calc_vec_metric_tensor, 0)
-        if len(Xnew.shape) == 1:
-            grad_vec_metric_tensor_wrt_Xnew = grad_func(Xnew)
-        else:
-            grad_vec_metric_tensor_wrt_Xnew = jax.vmap(grad_func)(Xnew)
-        return grad_vec_metric_tensor_wrt_Xnew
-
-    # def calc_vec_metric_tensor(self, Xnew, full_cov: bool = True):
-    #     # pos = pos.reshape(1, -1)
-    #     print("inside vec metric tensor Xnew")
-    #     print(Xnew.shape)
-    #     metric_tensor = self.metric_fn(Xnew, full_cov=full_cov)
-    #     input_dim = Xnew.shape[-1]
-    #     vec_metric_tensor = metric_tensor.reshape(
-    #         input_dim * input_dim,
-    #     )
-    #     return vec_metric_tensor
-
-    def metric_fn(
-        self,
-        Xnew: InputData,
-        full_cov: bool = True,
-    ) -> MetricTensor:
-        # jac_mean, jac_cov = gp_jacobian(
-        #     Xnew,
-        #     self.gp.inducing_variable,
-        #     self.gp.kernel,
-        #     self.gp.mean_function,
-        #     f=self.gp.q_mu,
-        #     full_cov=full_cov,
-        #     q_sqrt=self.gp.q_sqrt,
-        #     jitter=self.jitter,
-        #     white=self.gp.whiten,
-        # )
-        print('inside metric_fn')
-        print(Xnew.shape)
-        input_dim = Xnew.shape[-1]
-        if len(Xnew.shape) == 1:
-            # Xnew = Xnew.reshape(1, -1)
-            num_data = 1
-        else:
-            num_data = Xnew.shape[0]
-        print(Xnew.shape)
-
-        print("before gp.predict_jac what is Xnew.shape")
-        print(Xnew.shape)
-        jac_mean, jac_cov = self.gp.predict_jacobian_f_wrt_Xnew(
-            Xnew, full_cov=full_cov
-        )
-        print("jac_mean all")
-        print(jac_mean.shape)
-        print(jac_cov.shape)
-        print("num data")
-        print(num_data)
-        # TODO this is only true if full_cov=True
-        # assert jac_mean.shape == (num_data, input_dim, 1)
-        # assert jac_cov.shape == (num_data, input_dim, input_dim)
-
-        jac_mean_trans = leading_transpose(jac_mean, [..., -1, -2])
-        expected_jac_outer_prod = jnp.matmul(
-            jac_mean, jac_mean_trans
-        )  # [input_dim x input_dim]
-        print("expected_jac_outer_prod")
-        print(expected_jac_outer_prod.shape)
-        # assert expected_jac_outer_prod.shape == (
-        #     num_data,
-        #     input_dim,
-        #     input_dim,
-        # )
-
-        # jax.experimental.host_callback.id_print(expected_jac_outer_prod)
-        # expected_metric_tensor = expected_jac_outer_prod + cov_weight * output_dim * cov_j
-        expected_metric_tensor = (
-            expected_jac_outer_prod + self.covariance_weight * jac_cov
-        )
-        print("expected_metric_tensor")
-        print(expected_metric_tensor.shape)
-        # expected_metric_tensor = cov_weight * cov_j.T
-        # expected_metric_tensor = cov_j
-        # assert expected_metric_tensor.shape == (num_data, input_dim, input_dim)
-        return expected_metric_tensor
-
-        # f = self.gp.q_mu
-        # num_test_inputs = Xnew.shape[0]
-        # input_dim = Xnew.shape[1]
-        # output_dim = f.shape[1]
-
-        # expected_metric_tensor, mu_j, cov_j = jax.vmap(
-        #     calc_expected_metric_tensor_single, in_axes=(0)
-        # )(Xnew)
-        # return expected_metric_tensor, mu_j, cov_j
-        # return expected_metric_tensor
-
-    # def metric_fn(
-    #     self,
-    #     Xnew: InputData,
-    #     full_cov: bool = True,
-    # )-> MetricTensor:
-    #     def calc_expected_metric_tensor_single(x):
-    #         if len(x.shape) == 1:
-    #             x = x.reshape(1, input_dim)
-
-    #         mu_j, cov_j = gp_jacobian(
-    #             x,
-    #             self.gp.inducing_variable,
-    #             self.gp.kernel,
-    #             self.gp.mean_function,
-    #             f=self.gp.q_mu,
-    #             full_cov=full_cov,
-    #             # full_cov=False,
-    #             q_sqrt=self.gp.q_sqrt,
-    #             jitter=self.jitter,
-    #             white=self.gp.whiten,
-    #         )
-    #         # mu_j, cov_j = gp_jacobian_hard_coded(cov_fn, x, X, Y, jitter=jitter)
-    #         assert mu_j.shape == (input_dim, 1)
-    #         assert cov_j.shape == (input_dim, input_dim)
-
-    #         expected_jac_outer_prod = jnp.matmul(
-    #             mu_j, mu_j.T
-    #         )  # [input_dim x input_dim]
-    #         assert expected_jac_outer_prod.shape == (input_dim, input_dim)
-
-    #         # expected_metric_tensor = expected_jac_outer_prod + cov_weight * output_dim * cov_j
-    #         expected_metric_tensor = (
-    #             expected_jac_outer_prod + self.covariance_weight * cov_j
-    #         )
-    #         # expected_metric_tensor = cov_weight * cov_j.T
-    #         # expected_metric_tensor = cov_j
-    #         assert expected_metric_tensor.shape == (input_dim, input_dim)
-    #         return expected_metric_tensor, mu_j, cov_j
-
-    #     f = self.gp.q_mu
-    #     num_test_inputs = Xnew.shape[0]
-    #     input_dim = Xnew.shape[1]
-    #     output_dim = f.shape[1]
-
-    #     expected_metric_tensor, mu_j, cov_j = jax.vmap(
-    #         calc_expected_metric_tensor_single, in_axes=(0)
-    #     )(Xnew)
-    #     # return expected_metric_tensor, mu_j, cov_j
-    #     return expected_metric_tensor
-
-
-# class GPMetricTensor(RiemannianMetricTensor):
-#     def __init__(
-#         self,
-#         gp: GPBase,
-#         covariance_weight: jnp.float64 = 1.0,
-#         jitter: jnp.float64 = 1.0e-4,
-#     ):
-#         self.gp = gp
-#         self.covariance_weight = covariance_weight
-#         self.jitter = jitter
-
-#     def metric_fn(
-#         self,
-#         Xnew: InputData,
-#         full_cov: bool = True,
-#     ):
-#         def calc_expected_metric_tensor_single(x):
-#             if len(x.shape) == 1:
-#                 x = x.reshape(1, input_dim)
-
-#             mu_j, cov_j = gp_jacobian(
-#                 # Xnew,
-#                 x,
-#                 X,
-#                 kernel,
-#                 mean_func,
-#                 f=f,
-#                 full_cov=full_cov,
-#                 # full_cov=False,
-#                 q_sqrt=q_sqrt,
-#                 jitter=jitter,
-#                 white=True,
-#             )
-#             # mu_j, cov_j = gp_jacobian_hard_coded(cov_fn, x, X, Y, jitter=jitter)
-#             assert mu_j.shape == (input_dim, 1)
-#             assert cov_j.shape == (input_dim, input_dim)
-
-#             expected_jac_outer_prod = jnp.matmul(
-#                 mu_j, mu_j.T
-#             )  # [input_dim x input_dim]
-#             assert expected_jac_outer_prod.shape == (input_dim, input_dim)
-
-#             # expected_metric_tensor = expected_jac_outer_prod + cov_weight * output_dim * cov_j
-#             expected_metric_tensor = (
-#                 expected_jac_outer_prod + cov_weight * cov_j
-#             )
-#             # expected_metric_tensor = cov_weight * cov_j.T
-#             # expected_metric_tensor = cov_j
-#             assert expected_metric_tensor.shape == (input_dim, input_dim)
-#             return expected_metric_tensor, mu_j, cov_j
-
-#         num_test_inputs = Xnew.shape[0]
-#         input_dim = Xnew.shape[1]
-#         output_dim = f.shape[1]
-#         print("inside gp_metric_tensor")
-#         print(output_dim)
-
-#         expected_metric_tensor, mu_j, cov_j = jax.vmap(
-#             calc_expected_metric_tensor_single, in_axes=(0)
-#         )(Xnew)
-#         return expected_metric_tensor, mu_j, cov_j
-
-
-# class SVGPMetricTensor(GPMetricTensor):
-#     def __init__(
-# self,
-#     gp: GPBase,
-#     X: InputData,
-#     f: OutputData,
-#     covariance_weight: jnp.float64 = 1.0,
-#     q_sqrt=None,
-#     white: bool = True,
-#     jitter: jnp.float64 = 1.0e-4,
-# ):
-#     super().__init__(
-#         kernel=kernel,
-#         mean_function=mean_function,
-#         covariance_weight=covariance_weight,
-#         jitter=jitter,
-#     )
-#     full_cov = True
-
-# def metric_fn(
-#     self,
-#     Xnew: InputData,
-#     X: InputData,
-#     f: OutputData,
-# ):
-#     return 0
